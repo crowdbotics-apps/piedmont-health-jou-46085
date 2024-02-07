82937e5de7d3c0d9bb1c9e5037f717639c0981c9
Crowdbotics <team@crowdbotics.com>
2024-02-07 17:02:02 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "piedmont-health-jou-46085",
+  url: "https://piedmont-health-jou-46085.botics.co",
+  api: "https://piedmont-health-jou-46085.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
